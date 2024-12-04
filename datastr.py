3dca72640c1f7f942b8180ae553e66e88ce02872
yigedaigua <52713163+yigedaigua@users.noreply.github.com>
2024-12-04 16:14:29 +0800

    Update datastr.py
    新增文件内容显示和按照时间筛选数据

--- datastr.py
+++ datastr.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 def dataprocessing(data:str):
     try:
         if "#" in data:
@@ -9,5 +10,17 @@ def dataprocessing(data:str):
                 return float(data)
     except:
         return data
+
+def Screening_time(all_time:tuple,check_time:str) -> bool:
+    # 定义时间范围
+    start_time = all_time[0]
+    end_time = all_time[1]
+    time_obj = datetime.strptime(check_time, "%H:%M:%S")
+    is_within_range = start_time <= time_obj.time() <= end_time
+    if is_within_range:
+        return True
+    else:
+        return False
+
 if __name__ == "__main__":
     print(dataprocessing("16:23:32"))
